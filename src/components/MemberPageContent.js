cb1993a05ad4f72d0dd30b822f1b70ab16020b98
Martin Najjar <martin_najjar@yahoo.com>
2021-03-20 22:04:58 +0300

    Create a reusable MemberPageContent

---
+++ src/components/MemberPageContent.js
@@ -0,0 +1,70 @@
+import {
+  Card,
+  CardActionArea,
+  CardActions,
+  CardContent,
+  CardMedia,
+  makeStyles,
+  TextField,
+  Typography,
+} from '@material-ui/core';
+import PropTypes from 'prop-types';
+import React from 'react';
+import commonStyles from '../utils/commonStyles';
+
+const useStyles = makeStyles({
+  root: {
+    width: '90%',
+    maxWidth: 500,
+    boxShadow: 'none',
+    margin: '30px auto 0',
+  },
+  media: {
+    height: 140,
+  },
+});
+
+const MemberPageContent = ({ name, image, info }) => {
+  const classes = useStyles();
+  const commonClasses = commonStyles();
+  return (
+    <Card className={classes.root}>
+      <CardActionArea>
+        <CardMedia
+          className={classes.media}
+          image={image}
+        />
+        <CardContent>
+          <Typography gutterBottom color="textSecondary" variant="h5" component="h2">
+            {name}
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            {info}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <TextField
+          className={commonClasses.darkText}
+          fullWidth
+          value=""
+          name={name}
+          label={name}
+          required
+          variant="outlined"
+          helperText="Should be in Gram"
+          margin="dense"
+          type="number"
+        />
+      </CardActions>
+    </Card>
+  );
+};
+
+MemberPageContent.propTypes = {
+  name: PropTypes.string.isRequired,
+  image: PropTypes.string.isRequired,
+  info: PropTypes.string.isRequired,
+};
+
+export default MemberPageContent;
